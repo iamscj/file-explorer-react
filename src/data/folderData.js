5fd58296435ef32e05cf92786fa981976cdefb1a
Shreyasa Joshi <91722818+iamscj@users.noreply.github.com>
2024-09-08 08:01:58 +0000

    complete tree structure

---
+++ src/data/folderData.js
@@ -0,0 +1,64 @@
+export const explorer = {
+  id: "1",
+  name: "root",
+  isFolder: true,
+  items: [
+    {
+      id: "2",
+      name: "src",
+      isFolder: true,
+      items: [
+        {
+          id: "3",
+          name: "App.js",
+          isFolder: false,
+          items: [],
+        },
+        {
+          id: "4",
+          name: "components",
+          isFolder: true,
+          items: [
+            {
+              id: "5",
+              name: "Header.js",
+              isFolder: false,
+              items: [],
+            },
+            {
+              id: "6",
+              name: "Footer.js",
+              isFolder: false,
+              items: [],
+            },
+            {
+              id: "7",
+              name: "common",
+              isFolder: true,
+              items: [
+                {
+                  id: "8",
+                  name: "Button.js",
+                  isFolder: false,
+                  items: [],
+                },
+                {
+                  id: "9",
+                  name: "Input.js",
+                  isFolder: false,
+                  items: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: "10",
+      name: "package.json",
+      isFolder: false,
+      items: [],
+    },
+  ],
+};
